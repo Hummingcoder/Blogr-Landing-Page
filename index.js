6cbaf4b1b728a69478721cfd11537007da056e3d
Vishal D. <dwivedivishal0702@gmail.com>
2024-08-07 10:06:38 +0530

    nav fixing

---
+++ index.js
@@ -0,0 +1,46 @@
+const togglebtn = document.querySelector(".toggle");
+const nav = document.querySelector("nav");
+const dropBtn = document.querySelectorAll(".dropbtn");
+
+togglebtn.addEventListener("click", (e) => {
+  togglebtn.classList.toggle("show");
+  if (togglebtn.classList.contains("show")) {
+    nav.classList.remove("hide");
+  } else {
+    nav.classList.add("hide");
+    dropBtn.forEach((Btn) => {
+      const child_img = Btn.querySelectorAll("img");
+      Btn.nextElementSibling.style.height = "0";
+      Btn.nextElementSibling.style.padding = "0";
+      child_img.forEach((img) => {
+        img.style.transform = "rotate(0deg)";
+      });
+    });
+  }
+});
+let re = true;
+dropBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const img = btn.querySelectorAll("img");
+    const drop_container = btn.nextElementSibling;
+    dropBtn.forEach((Btn) => {
+      const child_img = Btn.querySelectorAll("img");
+      Btn.nextElementSibling.style.height = "0";
+      Btn.nextElementSibling.style.padding = "0";
+      child_img.forEach((img) => {
+        img.style.transform = "rotate(0deg)";
+      });
+    });
+
+    if (re) {
+      drop_container.style.padding = "1rem";
+      drop_container.style.height = "fit-content";
+      img.forEach((img) => {
+        img.style.transform = "rotate(180deg)";
+      });
+      re = false;
+    } else {
+      re = true;
+    }
+  });
+});
